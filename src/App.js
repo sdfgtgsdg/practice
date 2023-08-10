00a423caefcc0d55d17e02f9d61874d34626c99b
nh <hnm921125@gmail.com>
2023-08-10 14:23:59 +0900

    작업스

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom' ;
+import { styled } from 'styled-components';
+
+
+const Btn = styled.button`
+background: pink;
+font-size: 25px;
+`
 
 function App() {
   return (
     <div className="App">
+      <Btn>버튼</Btn>
       <div>헤더작업</div>
       <Routes>
         <Route path='/' element={<div>메인페이지</div>}/>
         <Route path='/new' element={<div>새글페이지</div>}/>
         <Route path='/edit' element={<div>수정페이지</div>}/>
       </Routes>
+      <div>Footer</div>
      </div>
   );
 }
